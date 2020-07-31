960d4ee5432dc952f7b259df3d5d6bde2cec7d55
Aluisio Anderson <aaluisio@hotmail.com>
2020-07-31 20:49:19 -0300

    Primeiro programa

---
+++ firstprogram.c
@@ -0,0 +1,12 @@
+//Esqueleto básico de um código em linguagem C
+//std = standard e io = input/output
+#include <stdio.h>
+
+int main() {
+
+    printf("Bem-vindo, quarto ano!");
+
+    /*Se retornar zero significa que o programa não deu erro.
+      Se ocorrer algum erro o programa retorna um valor diferente de zero*/
+    return 0;
+}
